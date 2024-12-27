a255e06fe409fa4be9ffdadbd7821a63fb62a36e
Emma Fewer <emmafewer20@gmail.com>
2024-12-27 13:47:00 -0700

    removed unused imports

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -2,7 +2,7 @@
 
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import {deleteInvoice, DeleteState, State} from '@/app/lib/actions';
+import {deleteInvoice} from '@/app/lib/actions';
 import {useActionState} from "react";
 
 export function CreateInvoice() {
