d75fed5ba02022c93df54681f65e25451059fe85
Emma Fewer <emmafewer20@gmail.com>
2024-12-27 14:28:17 -0700

    fixed lint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,7 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -95,7 +95,7 @@ export async function updateInvoice(
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Update invoice.",
     };
@@ -110,7 +110,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Delete invoice." };
   }
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -12,10 +12,7 @@ import { authenticate } from "@/app/lib/actions";
 import { useActionState } from "react";
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined,
-  );
+  const [errorMessage, formAction] = useActionState(authenticate, undefined);
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
